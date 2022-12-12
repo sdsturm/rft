ea1b607976dc08c57e1d23e7dce6a5a0daa9286d
Simon Sturm <sturmsimon@t-online.de>
2022-12-12 22:45:27 +0100

    Started some command line parsing

---
+++ test.c
@@ -0,0 +1,22 @@
+#include "config.h"
+#include "touchstone.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char const* argv[])
+{
+  const char* filename = argv[1];
+
+  int n_ports;
+  int n_freq;
+  double r_ref;
+  double* freq;
+  rft_complex* data;
+
+  parse_touchstone(filename, &n_ports, &n_freq, &r_ref, &freq, &data);
+
+  return EXIT_SUCCESS;
+}
