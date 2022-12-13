2667ce02f5c45dde8e49483b121935608a13ae57
Simon Sturm <sturmsimon@t-online.de>
2022-12-13 12:29:32 +0000

    Cosmetics

--- lib/tests/touchstone/main.cpp
+++ lib/tests/touchstone/main.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE tests_cmplx
+#define BOOST_TEST_MODULE tests_touchstone
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 // For linking to the shared library see
