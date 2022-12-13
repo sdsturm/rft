8162b7c8593e7a6aabf98aadf4454f2e0ea00098
Simon Sturm <sturmsimon@t-online.de>
2022-12-13 12:32:17 +0000

    Removed unit tests

--- lib/tests/touchstone/main.cpp
+++
@@ -1,5 +0,0 @@
-#define BOOST_TEST_MODULE tests_touchstone
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
-// For linking to the shared library see
-// boost.org/doc/libs/1_75_0/libs/test/doc/html/boost_test/usage_variants.html
